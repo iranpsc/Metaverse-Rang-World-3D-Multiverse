2ad7e2608ad61f6f203d7244e2464c5e3cd0b1a7
siyamak_unity <ssaadatpur@gmail.com>
2026-07-03 04:11:55 +0330

    edit script

--- Assets/Scripts/Core/CoroutineRunner.cs
+++ Assets/Scripts/Core/CoroutineRunner.cs
@@ -1,35 +1,59 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class CoroutineRunner : MonoBehaviour
 {
     private static CoroutineRunner _instance;
 
-    public static Coroutine Run(IEnumerator routine)
+    private static CoroutineRunner Instance
+    {
+        get
+        {
+            if (_instance != null) return _instance;
+
+            CoroutineRunner existingRunner = FindFirstObjectByType<CoroutineRunner>();
+            if (existingRunner != null)
+            {
+                _instance = existingRunner;
+                DontDestroyOnLoad(_instance.gameObject);
+                return _instance;
+            }
+
+            GameObject obj = new GameObject("CoroutineRunner");
+            _instance = obj.AddComponent<CoroutineRunner>();
+            DontDestroyOnLoad(obj);
+            return _instance;
+        }
+    }
+
+    private void Awake()
     {
         if (_instance == null)
         {
-            GameObject obj = new GameObject("CoroutineRunner");
-            _instance = obj.AddComponent<CoroutineRunner>();
-            GameObject.DontDestroyOnLoad(obj);
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
         }
 
-        return _instance.StartCoroutine(routine);
+        if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // اضافه به انتهای کلاس CoroutineRunner
+    public static Coroutine Run(IEnumerator routine)
+    {
+        if (routine == null) return null;
+        return Instance.StartCoroutine(routine);
+    }
+
     public static void Stop(Coroutine routine)
     {
-        if (_instance != null && routine != null)
-            _instance.StopCoroutine(routine);
+        if (_instance != null && routine != null) _instance.StopCoroutine(routine);
     }
 
     public static void StopAll()
     {
-        if (_instance != null)
-            _instance.StopAllCoroutines();
+        if (_instance != null) _instance.StopAllCoroutines();
     }
 }
